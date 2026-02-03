6dc8f4385cf1e69f2ac582184779cd6aa63f0acd
DARWIN.BESORIO16 <darwin.besorio16@gmail.com>
2026-02-03 09:46:16 +0800

    Added updated

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import SingleMovie from '../views/SingleMovie.vue';
 import AdminDashboard from '../views/AdminDashboard.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_API_BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: '/login' },
     { path: '/login', component: Login },
